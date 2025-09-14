7c3a0f985df622f72c1f5e9fd28c77a69a8b1bb8
beengineer-home-proxmox-vm103 <beengineer0420@gmail.com>
2025-09-14 16:04:37 +0900

    250914 | Start writing Containerfile & setup-bootc.sh

---
+++ week-1/250914/setup-bootc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+PKGS='podman bootc'
+
+echo '##### This is setup-bootc.sh! #####'
+
+echo
+echo
+
+echo '===== Step 1) Installing PKGS =====' 
+
+echo 'Updating dnf Package Manager'
+dnf update -y && echo 'Update complete!'
+
+echo "Installing PKGS: ${PKGS}"
+dnf install -y ${PKGS} && echo 'Installing complete!'
+
+echo
+echo
+
+echo '===== Step 2) Login Registry (quay.io)  =====' 
+podman login quay.io && echo 'Login complete!'
+
+echo
+echo
+
+
